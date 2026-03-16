8fbe8977132de64bd6ce02d9fd57158a4d4bdf3c
Kamil <kamil.dzietczyk1994@gmail.com>
2026-03-16 19:09:39 +0100

    implement batch upsert with execute_values

--- ingestion/pipelines/load_vehicle_makes.py
+++ ingestion/pipelines/load_vehicle_makes.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from psycopg2.extras import execute_values
+
 from ingestion.api.vehicle_api import fetch_vehicle_makes
 from ingestion.db.connection import get_connection
 
@@ -11,33 +13,34 @@ def run_pipeline():
 
     makes = fetch_vehicle_makes()
 
-    for make in makes:
-
-        cursor.execute(
-            """
-            INSERT INTO raw.api_vehicle_makes (make_id, make_name, created_at)
-            VALUES (%s, %s, %s)
-            ON CONFLICT (make_id)
-            DO UPDATE SET
-                make_name = EXCLUDED.make_name,
-                created_at = EXCLUDED.created_at
-            """,
-            (
-                make["Make_ID"],
-                make["Make_Name"],
-                datetime.utcnow()
-            )
+    records = [
+        (
+            make["Make_ID"],
+            make["Make_Name"],
+            None,
+            datetime.utcnow()
         )
+        for make in makes
+    ]
+
+    query = """
+        INSERT INTO raw.api_vehicle_makes (make_id, make_name, country, created_at)
+        VALUES %s
+        ON CONFLICT (make_id)
+        DO UPDATE SET
+            make_name = EXCLUDED.make_name,
+            created_at = EXCLUDED.created_at
+    """
+
+    execute_values(cursor, query, records)
 
     conn.commit()
 
     cursor.close()
     conn.close()
 
-    print(
-        f"Pipeline finished. Upserted {len(makes)} records into raw.api_vehicle_makes"
-    )
-
+    print(f"Pipeline finished. Upserted {len(records)} records.")
+    
 
 if __name__ == "__main__":
     run_pipeline()
