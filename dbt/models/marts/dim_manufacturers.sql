3e855d7b0421b630157da603d0677d2171d297dc
Kamil <kamil.dzietczyk1994@gmail.com>
2026-05-09 20:19:00 +0200

    improve dbt documentation and lineage configuration

--- dbt/models/marts/dim_manufacturers.sql
+++ dbt/models/marts/dim_manufacturers.sql
@@ -1,8 +1,17 @@
-SELECT DISTINCT
+WITH distinct_manufacturers AS (
+
+    SELECT DISTINCT
+        manufacturer
+
+    FROM {{ ref('stg_car_listings') }}
+
+    WHERE manufacturer IS NOT NULL
+
+)
+
+SELECT
     ROW_NUMBER() OVER (ORDER BY manufacturer) AS manufacturer_id,
 
     manufacturer AS manufacturer_name
 
-FROM {{ ref('stg_car_listings') }}
-
-WHERE manufacturer IS NOT NULL
+FROM distinct_manufacturers
